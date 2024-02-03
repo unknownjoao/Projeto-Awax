825a2c391692fcc686e10123561cf3fcc911af61
unknownjoao <jotalucasfarias@gmail.com>
2024-02-02 22:04:09 -0400

    projeto concluido! + js para abrir e fechar menu

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    document.querySelector(".menu-opener").addEventListener("click", function(){
+        if(document.querySelector("nav").style.display == 'flex') {
+            document.querySelector("nav").style.display = 'none';
+        } else {
+            document.querySelector("nav").style.display = 'flex';
+        }
+    });
+};
